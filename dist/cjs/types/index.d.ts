ecec0129b8a5a51bd208477425d6788c47dad392
Bhavya Kaushik <bkjyotsana9@gmail.com>
2025-09-13 07:12:16 +0530

    feat: added conventional commits and standard-version

--- dist/cjs/types/index.d.ts
+++ dist/cjs/types/index.d.ts
@@ -1,3 +1,3 @@
-export { default as Page } from './components/Page';
-import './index.css';
+export { default as Page } from "./components/Page";
+import "./index.css";
 //# sourceMappingURL=index.d.ts.map
